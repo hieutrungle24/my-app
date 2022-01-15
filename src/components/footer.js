cc45c75df236d01999d45de3974d9b0d0c94cb28
hieutrungle24 <hieucalm77@gmail.com>
2022-01-15 11:11:21 +0700

    update code lab_3

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 const Footer = {
     render() {
-        return /* html */ `<div class="max-w-5xl mx-auto bg-blue-800 py-4 text-center text-white"><p>Copyright by Datlt</p></div>`;
+        return /* html */ `<div class="max-w-5xl mx-auto bg-blue-800 py-4 text-center text-white"><p>Copyright by Hieult</p></div>`;
     },
 };
 export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,14 @@ const Header = {
     render() {
         return /* html */ `<nav class="max-w-5xl mx-auto bg-orange-500">
             <ul class="flex">
-                <li><a href="/" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Home Page</a></li>
-                <li><a href="/about" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">About Page</a></li>
-                <li><a href="/product" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Product Page</a></li>
+                <li><a href="/" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Trang chủ</a></li>
+                <li><a href="/about" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Tuyển sinh</a></li>
+                <li><a href="/product" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Chương trình đào tạo</a></li>
+                <li> <a href="/signup" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Sign up</a> </li>
+                <li> <a href="/signin" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Sign in</a> </li>
+                <li> <a href="/admin/dashboard" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Admin</a> </li>
+                <input type="search" class=" flex justify-end px-1 py-1 my-2 mr-5 mx-auto" placeholder="Tìm kiếm....">
+                <button class="border bg-blue-800 text-amber-50  my-2 ml-5 mx-auto hover:shadow-lg hover:border-pink-500 ">Tìm kiếm</button>
             </ul>
         </nav>`;
     },
